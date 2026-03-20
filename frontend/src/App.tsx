714785f1532c5e707a763f2910d425a5a24c93a3
jeevanvishnu <jeevanvishnu18@gmail.com>
2026-03-20 21:22:52 +0530

    feat: add resume upload page and update dashboard layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,10 @@ import AuthLayout from './layouts/AuthLayout';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import UploadResume from './pages/UploadResume';
 import GoogleAuthCallback from './pages/GoogleAuthCallback';
-import { useSelector } from 'react-redux';
 import type { RootState } from './app/Store';
+import { useSelector } from 'react-redux';
 
 function App() {
   const { user } = useSelector((state: RootState) => state.auth);
@@ -19,6 +20,7 @@ function App() {
         </Route>
         <Route path="/auth/google/callback" element={<GoogleAuthCallback />} />
         <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path='/upload-resume' element={user ? <UploadResume /> : <Navigate to="/login" />} />
         <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
